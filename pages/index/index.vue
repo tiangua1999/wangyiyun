3fe56838d52c321318a190c773d2c80a590897c4
guagua <2637931857@qq.com>
2023-04-20 19:21:43 +0800

    index,user,search

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="box">
+		<button style='width: 80%; background-color: #f5f5f5; height: 50rpx; padding-left: 10px;border-radius: 50px;' ></button>
 	</view>
 </template>
 
@@ -11,7 +8,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
 		onLoad() {
@@ -24,29 +21,6 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	@import './index.scss'
+	
 </style>
---
+++ pages/user/user.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="box">
+		nihao
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
