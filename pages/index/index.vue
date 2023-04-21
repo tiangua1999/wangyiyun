9b920e2f729ba2ba7916859d489ce2888cf8f059
guagua <2637931857@qq.com>
2023-04-21 15:53:24 +0800

    bbbb

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,26 +5,37 @@
 		</button>
 	</view>
 	<view class="list-box">
+		<text style='margin-bottom: 10px;display: inline-block;'>榜单：</text>
 		
 		<view class='item' v-for='i in hotList'>
 			<view >
 				<img class='item-img' :src="i.coverImgUrl" alt="">
 			</view>
-			<view >
-				
+			<view style='font-size: 12rpx'>
+				{{i.description}}
 			</view>
 			
 		</view>
 	</view>
+	<view class="list-box2">
+		<text style='margin-bottom: 10px;display: inline-block;'>热门歌单：</text>
+		<view class="item2-box">
+			<view class='item2' v-for='i in hotList2'>
+					{{i.name}}
+			</view>
+		</view>
+		
+	</view>
 </template>
 
 <script>
-	import  { getallbangdan } from '../../apis/apis.js'
+	import  { getallbangdan,getrmgd } from '../../apis/apis.js'
 	
 	export default {
 		data() {
 			return {
-				hotList:[]
+				hotList:[],
+				hotList2:[],
 			}
 		},
 		onLoad() {
@@ -37,6 +48,9 @@
 			console.log(res.data.list);
 			this.hotList = res.data.list.slice(0,3)
 			console.log(this.hotList);
+			let res2 = await getrmgd()
+			console.log(res2.data.tags);
+			this.hotList2 = res2.data.tags
 		},
 		goseach(){
 			uni.navigateTo({
--- pages/seach/seach.vue
+++ pages/seach/seach.vue
@@ -1,22 +1,61 @@
 <template>
-	<view>
-		搜索页面
+	<view class="box">
+		<input v-model='value' @confirm='seach' style='width: 70%; background-color:
+		 #f5f5f5; height: 50rpx; padding-left: 10px;
+		 border-radius: 50px;font-size: 12rpx;margin-right: 20rpx;' />
+		 <text @click='seach'>搜索</text>
+	</view>
+	
+	<view class="s-box">
+		<text style='font-size: 15px;font-weight: 600;'>搜索结果：</text>
+		<view class="center">
+			<view class="song" v-for="(item,index) in list">
+				<view class="left">
+					<view class='left-cent' style='font-size: 14px;color: cadetblue;'>{{item.name}}</view> 
+					<view class='left-cent'  style='font-size: 12px;'>{{item.ar[0].name}}-{{item.al.name}}</view>
+				</view>
+				<view @click='change(index)' class="right" :class='{zanting:index1===index}'>
+					
+				</view>
+			</view>
+		</view>
+		
 	</view>
 </template>
 
 <script>
+	import {getsousuo} from '../../apis/apis.js'
 	export default {
 		data() {
 			return {
-				
+				value:'',
+				list:[],
+				index1:''
 			}
 		},
-		methods: {
+		mounted() {
 			
+		},
+		methods: {
+			async seach(){
+				console.log(this.value);
+				let res = await getsousuo({keywords:this.value})
+				console.log(res);
+				this.list=res.data.result.songs
+			},
+			change(a){
+				console.log(a);
+				if(a===this.index1){
+					this.index1 = ''
+				}else{
+					this.index1 = a
+				}
+			}
 		}
 	}
 </script>
 
 <style>
+	@import './index.scss'
 
 </style>
